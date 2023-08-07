ed4493822dd12ad87e3bceb5c855a2b11ec5d748
kimhyemin9988 <rlagpals159@naver.com>
2023-08-07 19:23:52 +0900

    feat:Add subtext in Information 5
    Text appears only when Selection 3 is pressed.
    Text disappears for Selection 1 and Selection 2.

--- src/Components/CkBoxRadio.tsx
+++ src/Components/CkBoxRadio.tsx
@@ -1,25 +1,30 @@
 import { UseFormRegister } from "react-hook-form";
 import { FormI } from "./Form";
+import { useState } from "react";
 
-export interface CkBoxRadioI{
+export interface CkBoxRadioI {
     label: string;
     data: string | string[];
     type?: string;
-    register : UseFormRegister<FormI>;
+    register: UseFormRegister<FormI>;
 }
 
 const CkBoxRadio = ({ label, data, type, register }: CkBoxRadioI) => {
     const id = data as string[];
+    const [subText, setSubText] = useState(false);
 
     return (<>
         {id.map((ele) => {
             return (
                 <div key={ele + "a"}>
-                    <input type={type} id={ele + type} value={ele} {...register(label)}></input>
+                    <input type={type} id={ele + type} value={ele} {...register(label)} onClick={() => {
+                        setSubText((prev) => ele === "선택3" ? prev = true : prev = false)
+                    }}></input>
                     <label htmlFor={ele + type}>{ele}</label>
                 </div>
             )
         })}
+        {subText && <h1>*선택시 텍스트가 표시됩니다.</h1>}
     </>)
 }
 export default CkBoxRadio;
